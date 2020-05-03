cbfac1a83dd9c89436806b451816d0c9a2abb4a0
Pondpaunsevenz <pppangpondpp7@gmail.com>
2020-05-03 22:58:46 +0700

    added autoload

--- lib/spree_bank_transfer/engine.rb
+++ lib/spree_bank_transfer/engine.rb
@@ -4,6 +4,8 @@ module SpreeBankTransfer
     isolate_namespace Spree
     engine_name 'spree_bank_transfer'
 
+    config.autoload_paths += %W(#{config.root}/lib)
+
     # use rspec for tests
     config.generators do |g|
       g.test_framework :rspec
