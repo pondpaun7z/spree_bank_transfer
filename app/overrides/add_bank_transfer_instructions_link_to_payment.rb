df12832d58d47f806d04a4c7f9c735e0c4c389e9
Pondpaunsevenz <pppangpondpp7@gmail.com>
2020-05-04 00:08:49 +0700

    fix template

--- app/overrides/add_bank_transfer_instructions_link_to_payment.rb
+++ app/overrides/add_bank_transfer_instructions_link_to_payment.rb
@@ -2,9 +2,5 @@ Deface::Override.new(
   virtual_path: 'spree/checkout/_payment',
   name: 'add_bank_transfer_instructions_link_to_payment',
   insert_after: "#payment-method-fields",
-  text: %q{
-    <% if @order.available_payment_methods.any? { |pm| pm.type == 'Spree::Gateway::BankTransfer' } %>
-      <p><%= link_to t(:how_bank_transfer_works), bank_transfer_instructions_path, id: 'banktransfer_instructions', target: "_blank" %></p>
-    <% end %>
-  }
+  partial: "spree/checkout/payment/banktransfer"
 )
