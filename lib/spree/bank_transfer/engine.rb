d7e749044c1120bad3dab317f52a558a6fa2a4a4
Pondpaunsevenz <pppangpondpp7@gmail.com>
2020-05-03 21:16:17 +0700

    fix engie

--- lib/spree/bank_transfer/engine.rb
+++ lib/spree/bank_transfer/engine.rb
@@ -1,11 +1,11 @@
 module Spree
   module BankTransfer
     class Engine < Rails::Engine
-      engine_name 'bank-transfer'
+      engine_name 'spree-bank-transfer'
 
       isolate_namespace Spree::BankTransfer
 
-      initializer "spree.gateway.payment_methods", :after => "spree.register.payment_methods" do |app|
+      initializer "spree.spree-bank-transfer.payment_methods", :after => "spree.register.payment_methods" do |app|
         app.config.spree.payment_methods << Spree::Gateway::BankTransfer
       end
     end
