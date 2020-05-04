4886515aeb51cc5d39eaba07d3489da33c725749
Pondpaunsevenz <pppangpondpp7@gmail.com>
2020-05-04 13:52:16 +0700

    fix template

--- app/models/spree/payment_decorator.rb
+++ app/models/spree/payment_decorator.rb
@@ -2,7 +2,7 @@ Spree::Payment.class_eval do
   attr_accessor :validate_bank_details
 
   has_one_attached :receipt
-  validates :bank_name, :account_no, :transaction_reference_no, :deposited_on, :receipt, presence: true, if: :validate_bank_details
+  validates :bank_name, :deposited_on, :receipt, presence: true, if: :validate_bank_details
 
   scope :from_bank_transfer, -> { joins(:payment_method).where(spree_payment_methods: { type: 'Spree::PaymentMethod::BankTransfer' }) }
 
