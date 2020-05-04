efa9da2c3f9962c0e0dcdb45bcfbc8c384034945
Pondpaunsevenz <pppangpondpp7@gmail.com>
2020-05-04 13:03:58 +0700

    fix template

--- app/overrides/add_payment_reference_details.rb
+++ app/overrides/add_payment_reference_details.rb
@@ -27,42 +27,5 @@ Deface::Override.new(
         </table>
       </fieldset>
     <% end %>
-
-    <% if bank_transfer_payment = @order.payments.from_bank_transfer.first %>
-      <% unless bank_transfer_payment.details_submitted? %>
-        <br>
-        <%= link_to Spree.t(:add_details), "javascript:void(0);", class: 'button add_bt_details' %>
-      <% end %>
-      <%= form_for bank_transfer_payment, html: { class: "bt_payment_form", style: "#{bank_transfer_payment.details_submitted? ? '' : 'display:none;'}" } do |f| %>
-        <div class="form-group">
-          <%= f.label Spree.t(:deposited_on) %>
-          <%= f.text_field :deposited_on, disabled: bank_transfer_payment.details_submitted?, class: 'required_field form-control', 'field-name' => 'Deposited on' %>
-        </div>
-
-        <div class="form-group">
-          <%= f.label Spree.t(:bank_name) %>
-          <%= f.select :bank_name, options_for_select(Spree::Bank.all.map { |x| [x.name, x.id] }) disabled: bank_transfer_payment.details_submitted?, class: 'required_field form-control', 'field-name' => 'Bank name' %>
-        </div>
-
-        <div class="form-group">
-          <%= f.label Spree.t(:account_no) %>
-          <%= f.text_field :account_no, disabled: bank_transfer_payment.details_submitted?, class: 'required_field form-control', 'field-name' => 'Account No.' %>
-        </div>
-
-        <div class="form-group">
-          <%= f.label Spree.t(:transaction_reference_no) %>
-          <%= f.text_field :transaction_reference_no, disabled: bank_transfer_payment.details_submitted?, class: 'required_field form-control', 'field-name' => 'Transaction Reference No.' %>
-        </div>
-
-        <div class="form-group">
-          <%= f.label Spree.t(:receipt) %>
-          <%= f.text_field :receipt, disabled: bank_transfer_payment.details_submitted?, class: 'required_field form-control', 'field-name' => 'Receipt' %>
-        </div>
-
-        <% unless bank_transfer_payment.details_submitted? %>
-          <%= f.submit "Submit", confirm: Spree.t(:confirm_details_submit), class: "btn btn-success btn-block" %>
-        <% end %>
-      <% end %>
-    <% end %>
   }
 )
